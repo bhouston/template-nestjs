a115dba73946fb2d6bdac37f4ca6a741b6bf988a
Ben Houston <bhouston@threekit.com>
2024-08-13 09:59:53 -0500

    swagger working, global validation working, move into /api route.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { OrgsModule } from './orgs/orgs.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, OrgsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,42 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+const initializeGlobalValidation = (app: INestApplication<any>) => {
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+};
+
+const initializeSwagger = (app: INestApplication<any>) => {
+  // Create Swagger options
+  const config = new DocumentBuilder()
+    .setTitle('My API')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+
+  // Create Swagger document
+  const document = SwaggerModule.createDocument(app, config);
+
+  // Set up the Swagger module
+  SwaggerModule.setup('api-docs', app, document);
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  initializeGlobalValidation(app);
+  app.setGlobalPrefix('api');
+
+  initializeSwagger(app);
+
   await app.listen(3000);
 }
 bootstrap();
